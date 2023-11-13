8510124f23267bc220a17a5a95069bcdec5df253
Harikesh Singh <2000079303@hexaware.com>
2023-11-13 12:10:58 +0530

    adding these two files

--- vars/kubeDelete.groovy
+++ vars/kubeDelete.groovy
@@ -1,5 +1,5 @@
 def call() {
    withKubeConfig(caCertificate: '', clusterName: '', contextName: '', credentialsId: 'k8s', namespace: '', restrictKubeConfigAccess: false, serverUrl: '') {
-    sh "kubectl delete -f deployment.yml"
+    sh "kubectl delete -f https://github.com/Aj7Ay/Youtube-clone-app/blob/main/deployment.yml"
 }
 }
